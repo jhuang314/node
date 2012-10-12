d56ea6d5c47d59896457e302a3bc3a89ab1ae619
Jesse Huang <jeshuang@ruslug.rutgers.edu>
2012-10-12 13:21:50 -0400

    finished post handling

--- server.js
+++ server.js
@@ -3,15 +3,21 @@ var url = require("url");
 
 function start(route, handle) {
 	function onRequest(request, response) {
+		var postData = "";
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received.");
+		request.setEncoding("utf8");
 
-		route(handle, pathname, response);
+		request.addListener("data", function(postDataChunk) {
+				postData += postDataChunk;
+				console.log("Received POST data chunk '" +
+					postDataChunk + "'.");
+				});
 
-		//response.writeHead(200, {"Content-Type" : "text/plain"});
-		//var content = route(handle, pathname);
-		//response.write("Hello World");
-		//response.end();
+		request.addListener("end", function() {
+				route(handle, pathname, response, postData);
+				});
+
 	}
 
 	http.createServer(onRequest).listen(8888);
